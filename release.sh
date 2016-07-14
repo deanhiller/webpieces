7c9be6c1c63c5e0fb942080323a20a7b5b7e571a
Dean Hiller <dean.hiller@gmail.com>
2016-07-13 22:21:25 -0600

    tweak the release to get it working

--- release.sh
+++ release.sh
@@ -3,4 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR
-./gradlew -PprojVersion=$1 clean release
+#MUST turn parallel builds off for release or it fails!!!
+./gradlew -Dorg.gradle.parallel=false -Dorg.gradle.configureondemand=false -PprojVersion=$1 clean release
