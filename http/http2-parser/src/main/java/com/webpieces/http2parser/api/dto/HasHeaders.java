2ab68557d23a744f6ca88c994565800744145640
Sameer Parekh Brenn <sameer@creativedestruction.com>
2016-10-04 14:14:16 -0600

    add simple test for hpack

--- http/http2-parser/src/main/java/com/webpieces/http2parser/api/dto/HasHeaders.java
+++ http/http2-parser/src/main/java/com/webpieces/http2parser/api/dto/HasHeaders.java
@@ -7,6 +7,25 @@ import java.util.List;
 
 public interface HasHeaders {
     class Header {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Header header1 = (Header) o;
+
+            if (header != null ? !header.equals(header1.header) : header1.header != null) return false;
+            return value != null ? value.equals(header1.value) : header1.value == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = header != null ? header.hashCode() : 0;
+            result = 31 * result + (value != null ? value.hashCode() : 0);
+            return result;
+        }
+
         public Header(String header, String value) {
             this.header = header;
             this.value = value;
--- http/http2-parser/src/test/java/com/webpieces/http2parser/TestHttp2Parser.java
+++ http/http2-parser/src/test/java/com/webpieces/http2parser/TestHttp2Parser.java
@@ -2,6 +2,7 @@ package com.webpieces.http2parser;
 
 import com.webpieces.http2parser.api.Http2Parser;
 import com.webpieces.http2parser.api.Http2ParserFactory;
+import com.webpieces.http2parser.api.dto.HasHeaders;
 import com.webpieces.http2parser.api.dto.Http2Frame;
 import com.webpieces.http2parser.api.ParserResult;
 import org.junit.Assert;
@@ -13,6 +14,7 @@ import org.webpieces.data.api.DataWrapperGenerator;
 import org.webpieces.data.api.DataWrapperGeneratorFactory;
 
 import javax.xml.crypto.Data;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TestHttp2Parser {
@@ -51,6 +53,30 @@ public class TestHttp2Parser {
     private static DataWrapperGenerator dataGen = DataWrapperGeneratorFactory.createDataWrapperGenerator();
 
     private static Http2Parser parser = Http2ParserFactory.createParser(new BufferCreationPool());
+
+    private static LinkedList<HasHeaders.Header> basicRequestHeaders = new LinkedList<>();
+    private static LinkedList<HasHeaders.Header> basicResponseHeaders = new LinkedList<>();
+
+    // https://github.com/http2jp/hpack-test-case/blob/master/nghttp2/story_00.json
+    private static String basicRequestSerializationNghttp2 = "82864188f439ce75c875fa5784";
+    static {
+        basicRequestHeaders.add(new HasHeaders.Header(":method", "GET"));
+        basicRequestHeaders.add(new HasHeaders.Header(":scheme", "http"));
+        basicRequestHeaders.add(new HasHeaders.Header(":authority", "yahoo.co.jp"));
+        basicRequestHeaders.add(new HasHeaders.Header(":path", "/"));
+
+        basicResponseHeaders.add(new HasHeaders.Header(":status", "200"));
+        basicResponseHeaders.add(new HasHeaders.Header("date", "Tue, 27 Sep 2016 19:41:50 GMT"));
+        basicResponseHeaders.add(new HasHeaders.Header("content-type", "text/html"));
+        basicResponseHeaders.add(new HasHeaders.Header("set-cookie", "__cfduid=d8bfe297ef26ef6252ea3a822360a6f411475005310; expires=Wed, 27-Sep-17 19:41:50 GMT; path=/; domain=.cloudflare.com; HttpOnly"));
+        basicResponseHeaders.add(new HasHeaders.Header("last-modified", "Tue, 27 Sep 2016 17:39:01 GMT"));
+        basicResponseHeaders.add(new HasHeaders.Header("cache-control", "public, max-age=14400"));
+        basicResponseHeaders.add(new HasHeaders.Header("served-in-seconds", "0.001"));
+        basicResponseHeaders.add(new HasHeaders.Header("cf-cache-status", "REVALIDATED"));
+        basicResponseHeaders.add(new HasHeaders.Header("expires", "Tue, 27 Sep 2016 23:41:50 GMT"));
+        basicResponseHeaders.add(new HasHeaders.Header("server", "cloudflare-nginx"));
+        basicResponseHeaders.add(new HasHeaders.Header("cf-ray", "2e916f776c724fd5-DEN"));
+    }
 
     @Test
     public void testBasicParse() {
@@ -135,7 +161,18 @@ public class TestHttp2Parser {
     }
 
     @Test
-    public void testCreateSerializedHeaders() {
+    public void testSerializeHeaders() {
+        DataWrapper serialized = parser.serializedHeaders(basicRequestHeaders);
+        String serializedHex = UtilsForTest.toHexString(serialized.createByteArray());
+        Assert.assertArrayEquals(
+                UtilsForTest.toByteArray(serializedHex),
+                UtilsForTest.toByteArray(basicRequestSerializationNghttp2)
+        );
+    }
 
+    @Test
+    public void testDeserializeHeaders() {
+        List<HasHeaders.Header> headers = parser.deserializeHeaders(UtilsForTest.dataWrapperFromHex(basicRequestSerializationNghttp2));
+        Assert.assertEquals(headers, basicRequestHeaders);
     }
 }
