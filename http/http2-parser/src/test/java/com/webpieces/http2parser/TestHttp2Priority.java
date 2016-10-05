5a20dd272be18ac82dd89e2389232c46cd8e1287
Sameer Parekh Brenn <sameer@creativedestruction.com>
2016-10-04 22:41:27 -0600

    add a test for unsigned fun

--- http/http2-parser/src/test/java/com/webpieces/http2parser/TestHttp2Priority.java
+++ http/http2-parser/src/test/java/com/webpieces/http2parser/TestHttp2Priority.java
@@ -14,12 +14,20 @@ public class TestHttp2Priority {
             "80 00 00 04" + // stream dependency
             "05"; // weight
 
+    static private String priorityFrameMSB =
+            "00 00 05" + //length
+                    "02" + //type
+                    "00" + //flags
+                    "00 00 00 00" + // R + streamid
+                    "80 00 00 04" + // stream dependency
+                    "FF"; // weight
+
     @Test
     public void testCreatePriorityFrame() {
         Http2Priority frame = new Http2Priority();
         frame.setStreamDependency(4);
         frame.setStreamDependencyIsExclusive(true);
-        frame.setWeight((byte) 0x5);
+        frame.setWeight((short) 0x5);
 
         String hexFrame = UtilsForTest.frameToHex(frame);
         Assert.assertArrayEquals(UtilsForTest.toByteArray(hexFrame), UtilsForTest.toByteArray(priorityFrame));
@@ -36,5 +44,27 @@ public class TestHttp2Priority {
 
         Assert.assertEquals(castFrame.getStreamDependency(), 4);
         Assert.assertTrue(castFrame.isStreamDependencyIsExclusive());
+    }
+
+    @Test
+    public void testCreatePriorityFrameMSB() {
+        Http2Priority frame = new Http2Priority();
+        frame.setStreamDependency(4);
+        frame.setStreamDependencyIsExclusive(true);
+        frame.setWeight((short) 0xFF);
+        Assert.assertEquals(frame.getWeight(), 255);
+        String hexFrame = UtilsForTest.frameToHex(frame);
+        Assert.assertArrayEquals(UtilsForTest.toByteArray(hexFrame), UtilsForTest.toByteArray(priorityFrameMSB));
+
+        UtilsForTest.testBidiFromBytes(hexFrame);
+    }
+
+    @Test
+    public void testParsePriorityFrameMSB() {
+        Http2Frame frame = UtilsForTest.frameFromHex(priorityFrameMSB);
+        Assert.assertTrue(Http2Priority.class.isInstance(frame));
+
+        Http2Priority castFrame = (Http2Priority) frame;
+        Assert.assertEquals(castFrame.getWeight(), 255);
     }
 }
