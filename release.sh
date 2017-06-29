fd4f99895433bec36ef185f48f651ff0aa93f362
Dean Hiller <dean.hiller@gmail.com>
2017-06-28 19:45:07 -0600

    tweak release script a bit

--- release.sh
+++ release.sh
@@ -4,16 +4,20 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR
 
-./runAllTesting.sh
+time ./runAllTesting.sh
 test_result=$?
 if [ $test_result -eq 0 ]
 then
+  echo "##################################"
   echo "Successfully RAN ALL TESTING $?"
+  echo "##################################"
 else
+  echo "##################################"
   echo "TESTING SYSTEM Failed(don't release this) $test_result"
+  echo "##################################"
   exit $test_result
 fi
 
 #MUST turn parallel builds off for release or it fails!!!
 #We skip tests since those are done in runAllTesting.sh AND that script ALSO test legacy compatibility and building a fake project from the new release AND starting the server
-./gradlew --stacktrace -Dorg.gradle.parallel=false -Dorg.gradle.configureondemand=false -PprojVersion=$@ clean release -x test
+time ./gradlew --stacktrace -Dorg.gradle.parallel=false -Dorg.gradle.configureondemand=false -PprojVersion=$@ clean release -x test
