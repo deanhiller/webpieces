445a4f7e772f77f92580a9e03b0aba4722ef828b
Dean Hiller <dean.hiller@gmail.com>
2017-07-20 18:35:59 -0600

    add tagging git in release script

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR
 
-time ./runAllTesting.sh
+./runAllTesting.sh
 test_result=$?
 if [ $test_result -eq 0 ]
 then
@@ -22,7 +22,21 @@ fi
 
 #MUST turn parallel builds off for release or it fails!!!
 #We skip tests since those are done in runAllTesting.sh AND that script ALSO test legacy compatibility and building a fake project from the new release AND starting the server
-time ./gradlew --stacktrace -Dorg.gradle.parallel=false -Dorg.gradle.configureondemand=false -PprojVersion=$@ clean release -x test
+./gradlew --stacktrace -Dorg.gradle.parallel=false -Dorg.gradle.configureondemand=false -PprojVersion=$@ clean release -x test
+test_result=$?
+if [ $test_result -eq 0 ]
+then
+  echo "##################################"
+  echo "RELEASE DONE, tagging git next"
+  echo "##################################"
+else
+  echo "##################################"
+  echo "RELEASE FAIL, not tagging git $test_result"
+  echo "##################################"
+  exit $test_result
+fi 
+
+git tag v1.9.$@
 
 end=`date +%s`
 runtime=$((end-start))
