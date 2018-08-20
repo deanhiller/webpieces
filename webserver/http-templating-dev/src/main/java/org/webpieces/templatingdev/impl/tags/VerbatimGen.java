d87e12beecee2f96c71c1bea6f090e73f8cb0e53
Dean Hiller <dean.hiller@gmail.com>
2018-08-19 18:41:04 -0600

    more changes

--- webserver/http-templating-dev/src/main/java/org/webpieces/templatingdev/impl/tags/VerbatimGen.java
+++ webserver/http-templating-dev/src/main/java/org/webpieces/templatingdev/impl/tags/VerbatimGen.java
@@ -4,13 +4,6 @@ import org.webpieces.templatingdev.api.AbstractTag;
 import org.webpieces.templatingdev.api.ScriptOutput;
 import org.webpieces.templatingdev.api.Token;
 
-/**
- * Deprecated so instead use *[ ]* which escapes html and templating tokens
- * 
- * @author dhiller
- *
- */
-@Deprecated
 public class VerbatimGen extends AbstractTag {
 
 	@Override
