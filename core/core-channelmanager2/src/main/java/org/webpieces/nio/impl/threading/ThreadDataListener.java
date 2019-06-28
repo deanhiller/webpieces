5cc6b011f8a04438c423173126b30faf99a7f9d7
Dean Hiller <dean.hiller@gmail.com>
2019-06-27 21:42:06 -0600

    fix checkstyle

--- core/core-channelmanager2/src/main/java/org/webpieces/nio/impl/threading/ThreadDataListener.java
+++ core/core-channelmanager2/src/main/java/org/webpieces/nio/impl/threading/ThreadDataListener.java
@@ -24,36 +24,47 @@ public class ThreadDataListener implements DataListener {
 	@Override
 	public CompletableFuture<Void> incomingData(Channel channel, ByteBuffer b) {
 		CompletableFuture<Void> future = new CompletableFuture<Void>();
-		executor.execute(channel, new Runnable() {
-			@Override
-			public void run() {
-				MDC.put("socket", ""+channel);
-				try {
-					
-					CompletableFuture<Void> fut = dataListener.incomingData(channel, b);
-					fut.handle((v, t) -> {
-						if(t == null)
-							future.complete(null);
-						else
-							future.completeExceptionally(t);
-						return null;
-					});
-					
-				} catch(Throwable e) {
-					log.error("Uncaught Exception", e);
-					future.completeExceptionally(e);
-				} finally {
-					MDC.clear();
-				}
-				
-
-
-			}
-		});
+		executor.execute(channel, new DataListeneRunanble(dataListener, channel, b, future));
 		
 		return future;
 	}
 
+	private static class DataListeneRunanble implements Runnable {
+		private DataListener dataListener;
+		private Channel channel;
+		private ByteBuffer buffer;
+		private CompletableFuture<Void> future;
+
+		public DataListeneRunanble(DataListener dataListener, Channel channel, ByteBuffer b,
+				CompletableFuture<Void> future) {
+					this.dataListener = dataListener;
+					this.channel = channel;
+					this.buffer = b;
+					this.future = future;
+		}
+
+		@Override
+		public void run() {
+			MDC.put("socket", ""+channel);
+			try {
+				CompletableFuture<Void> fut = dataListener.incomingData(channel, buffer);
+				fut.handle((v, t) -> {
+					if(t == null)
+						future.complete(null);
+					else
+						future.completeExceptionally(t);
+					return null;
+				});
+				
+			} catch(Throwable e) {
+				log.error("Uncaught Exception", e);
+				future.completeExceptionally(e);
+			} finally {
+				MDC.clear();
+			}
+		}
+	}
+	
 	@Override
 	public void farEndClosed(Channel channel) {
 		executor.execute(channel, new Runnable() {
