89741262af1eb9e4f6d9940a0ff626c05303d1b9
Dean Hiller <dean.hiller@gmail.com>
2020-02-11 10:04:01 -0700

    minor tweaks to translation

--- http/http2to1_1-client/src/main/java/org/webpieces/httpclientx/impl/Translations2.java
+++ http/http2to1_1-client/src/main/java/org/webpieces/httpclientx/impl/Translations2.java
@@ -8,20 +8,27 @@ import org.webpieces.http2client.api.dto.FullResponse;
 import org.webpieces.httpclient11.api.HttpFullRequest;
 import org.webpieces.httpclient11.api.HttpFullResponse;
 import org.webpieces.httpparser.api.common.Header;
+import org.webpieces.httpparser.api.common.KnownHeaderName;
 import org.webpieces.httpparser.api.dto.*;
 
 public class Translations2 {
 
 	public static HttpFullRequest translate(FullRequest request) {
 		//String scheme = request.getHeaders().getScheme();
-		//String authority = request.getHeaders().getAuthority();
+		String authority = request.getHeaders().getAuthority();
 		String path = request.getHeaders().getPath();
+		String methodString = request.getHeaders().getMethodString();
 		if(!path.startsWith("/"))
 			throw new IllegalArgumentException("http2 request :path header must start with /");
+		else if(methodString == null)
+			throw new IllegalArgumentException("http2 :method header is required");
+		else if(authority == null) {
+			throw new IllegalArgumentException("http1 required host header so http2 message must have :authority header set");
+		}
 
 		HttpRequestLine reqLine = new HttpRequestLine();
 		reqLine.setUri(new HttpUri(path));
-		reqLine.setMethod(new HttpRequestMethod(request.getHeaders().getMethodString()));
+		reqLine.setMethod(new HttpRequestMethod(methodString));
 		reqLine.setVersion(new HttpVersion());
 
 		HttpRequest httpReq = new HttpRequest();
@@ -32,6 +39,8 @@ public class Translations2 {
 		for(Http2Header header : request.getHeaders().getHeaders()) {
 			httpReq.addHeader(new Header(header.getName(), header.getValue()));
 		}
+
+		httpReq.addHeader(new Header(KnownHeaderName.HOST, authority));
 
 		HttpFullRequest req = new HttpFullRequest(httpReq, data);
 		return req;
