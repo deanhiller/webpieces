9feca174b174a8a741b9fa7253b9e05fe92bc1fd
Dean Hiller <dean.hiller@gmail.com>
2020-02-11 07:47:02 -0700

    fix jacoco since the upgrade

--- http/http2to1_1-client/src/main/java/org/webpieces/httpclientx/impl/Translations2.java
+++ http/http2to1_1-client/src/main/java/org/webpieces/httpclientx/impl/Translations2.java
@@ -1,14 +1,19 @@
 package org.webpieces.httpclientx.impl;
 
+import org.webpieces.data.api.DataWrapper;
 import org.webpieces.http2client.api.dto.FullRequest;
 import org.webpieces.http2client.api.dto.FullResponse;
 import org.webpieces.httpclient11.api.HttpFullRequest;
 import org.webpieces.httpclient11.api.HttpFullResponse;
+import org.webpieces.httpparser.api.dto.HttpRequest;
 
 public class Translations2 {
 
 	public static HttpFullRequest translate(FullRequest request) {
-		throw new UnsupportedOperationException("need to implement");
+		HttpRequest httpReq = new HttpRequest();
+		DataWrapper data = request.getPayload();
+		HttpFullRequest req = new HttpFullRequest(httpReq, data);
+		return null;
 	}
 
 	public static FullResponse translate(HttpFullResponse r) {
