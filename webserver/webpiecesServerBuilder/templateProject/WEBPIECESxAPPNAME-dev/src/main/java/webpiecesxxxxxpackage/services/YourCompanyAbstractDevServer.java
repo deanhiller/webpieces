d596f012be3474618dd8d3a0c6e4b6d893cebd13
Dean Hiller <dean.hiller@gmail.com>
2020-06-06 00:24:51 -0600

    shutdown dev server if platform is upgraded so there is no intermittent weird errors when specific jars are changed out

--- webserver/webpiecesServerBuilder/templateProject/WEBPIECESxAPPNAME-dev/src/main/java/webpiecesxxxxxpackage/services/YourCompanyAbstractDevServer.java
+++ webserver/webpiecesServerBuilder/templateProject/WEBPIECESxAPPNAME-dev/src/main/java/webpiecesxxxxxpackage/services/YourCompanyAbstractDevServer.java
@@ -1,10 +1,25 @@
 package webpiecesxxxxxpackage.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.webpieces.util.file.VirtualFile;
 import org.webpieces.webserver.api.IDESupport;
+import org.webpieces.webserver.api.WebpiecesServer;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 /**
  * Goes in re-usable location so all your dev servers can be modified
@@ -14,14 +29,26 @@ import org.webpieces.webserver.api.IDESupport;
  */
 public abstract class YourCompanyAbstractDevServer {
 
+	private static final Logger log = LoggerFactory.getLogger(YourCompanyAbstractDevServer.class);
     protected VirtualFile directory;
     protected ArrayList<VirtualFile> srcPaths;
     protected String[] args;
+
+    private ExecutorService fileWatchThread = Executors.newFixedThreadPool(1, new MyFileWatchThreadFactory());
 
     public YourCompanyAbstractDevServer(
             String name,
             boolean usePortZero
     ) {
+        //In DevServer or ProdServerForIDE, if platform is upgraded, bad things can happen so we shutdown on platform upgrade
+        //if we are currently running.
+        String file = "/"+ WebpiecesServer.class.getName().replaceAll("\\.", "/")+".class";
+        URL res = getClass().getResource(file);
+        if (res.getProtocol().equals("jar")) {
+            watchForDangerousJarChanges(res);
+        }
+    	
+    	
         DevConfig config = getConfig();
         
         directory = IDESupport.modifyForIDE(name);
@@ -85,4 +112,65 @@ public abstract class YourCompanyAbstractDevServer {
     protected abstract DevConfig getConfig();
 
     public abstract void start();
+    
+    private void watchForDangerousJarChanges(URL res) {
+        try {
+            watchForDangerousJarChangesImpl(res);
+        } catch (IOException e) {
+            throw new RuntimeException("Weird but this code can be safely removed BUT just make sure you restart your servers when upgrading any 3rd party jars");
+        }
+    }
+
+    private void watchForDangerousJarChangesImpl(URL res) throws IOException {
+        //It's always a jar in your code but sometimes we run Dev Server in webpieces where the code is not a jar
+        //ie(you can delete the if statement if you like)
+        log.info("res="+res.getFile()+" res="+res+" res1"+res.getPath());
+        //register jar file listener so on changes, we shutdown the server on the developer and make them reboot
+        String filePath = res.getFile();
+        String absPath = filePath.substring("file:".length());
+        String fullJarPath = absPath.split("!")[0];
+        File f = new File(fullJarPath);
+        Path directoryPath = f.getParentFile().toPath();
+
+        WatchService watcher = FileSystems.getDefault().newWatchService();
+        directoryPath.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        fileWatchThread.execute(new MyFileWatchRunnable(watcher));
+    }
+    
+    private class MyFileWatchRunnable implements Runnable {
+
+        private WatchService watcher;
+
+        public MyFileWatchRunnable(WatchService watcher) {
+            this.watcher = watcher;
+        }
+
+        @Override
+        public void run() {
+            WatchKey key;
+            try {
+                log.info("Starting to watch files");
+                // wait for a key to be available
+                key = watcher.take();
+            } catch (Throwable ex) {
+                log.error("Exception", ex);
+            }
+
+            log.error("\n-------------------------------------------------------------------------------\n"
+                + "Webpiecees was upgraded so we need to shutdown the server to use the new jar files or bad things happen\n"
+                +"-------------------------------------------------------------------------------");
+
+            System.exit(9492);
+        }
+    }
+
+    private class MyFileWatchThreadFactory implements ThreadFactory {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            t.setName("fileWatchThread");
+            return t;
+        }
+    }
 }
