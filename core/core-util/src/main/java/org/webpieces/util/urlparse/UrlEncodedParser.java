188db5e567356a9fdb05dcc4bb6559d4c95e2e7b
Dean Hiller <dean.hiller@gmail.com>
2020-06-18 19:02:34 -0600

    fix this

--- core/core-util/src/main/java/org/webpieces/util/urlparse/UrlEncodedParser.java
+++ core/core-util/src/main/java/org/webpieces/util/urlparse/UrlEncodedParser.java
@@ -3,6 +3,8 @@ package org.webpieces.util.urlparse;
 import java.nio.charset.StandardCharsets;
 import java.util.function.BiFunction;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.webpieces.util.net.URLEncoder;
 
 /**
@@ -10,6 +12,8 @@ import org.webpieces.util.net.URLEncoder;
  * the router dependent on anyones http parser so it can be reused
  */
 public class UrlEncodedParser {
+	
+	private static final Logger log = LoggerFactory.getLogger(UrlEncodedParser.class);
 	
 	public void parse(String multiPartData, BiFunction<String, String, Void> mapAddFunction) {
 	    String[] pairs = multiPartData.split("\\&");
@@ -24,8 +28,15 @@ public class UrlEncodedParser {
 	      
 	      //BUT this seems to break something else!!
 	      String value = null;
-	      if(fields.length == 2)
-	    	  value = URLEncoder.decode(fields[1], StandardCharsets.UTF_8);
+	      if(fields.length == 2) {
+	    	  try {
+	    		  value = URLEncoder.decode(fields[1], StandardCharsets.UTF_8);
+	    	  } catch(IllegalArgumentException e) {
+	    		  //thrown when %3D is last piece of value BUT is chopped to %3D.  I don't like catching this
+	    		  log.info("Some client fed a bad query param that can't be url decoded="+value);
+	    		  value = fields[1]; //don't decode bad data so json request still works and it fails on lookup in DB or whatnot
+	    	  }
+	      }
 	      
 	      mapAddFunction.apply(name, value);
 	    }
