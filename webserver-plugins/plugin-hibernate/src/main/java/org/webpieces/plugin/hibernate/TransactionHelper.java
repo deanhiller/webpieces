66a2e9d2dc67b77d6e690aadbe59ce968a075fed
Dean Hiller <dean.hiller@gmail.com>
2020-10-15 07:46:57 -0600

    add to transaction helper

--- webserver-plugins/plugin-hibernate/src/main/java/org/webpieces/plugin/hibernate/TransactionHelper.java
+++ webserver-plugins/plugin-hibernate/src/main/java/org/webpieces/plugin/hibernate/TransactionHelper.java
@@ -1,6 +1,7 @@
 package org.webpieces.plugin.hibernate;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -33,6 +34,45 @@ public class TransactionHelper {
 			Em.set(null);
 		}
 	}
+
+	public <Resp> Resp runWithEm(Supplier<Resp> function) {
+		EntityManager mgr = factory.createEntityManager();
+		
+		Em.set(mgr);
+		try {
+			Resp resp = function.get();
+			mgr.close();	
+			return resp;
+		} catch(RuntimeException e) {
+			tryClose(mgr, e);
+			throw e;
+		} finally {
+			Em.set(null);
+		}
+	}
+	
+	public <Resp> Resp runTransaction(EntityManager mgr, Supplier<Resp> function) {
+		EntityTransaction tx = mgr.getTransaction();
+
+		if(tx.isActive()) {
+			throw new IllegalStateException("You cannot call runTransaction in a transaction.  Call this method inside the function you pass to runWithEm.");
+		}
+		
+		tx.begin();
+		
+		Em.set(mgr);
+		
+		try {
+			Resp resp = function.get();
+			txCompleters.commit(tx, mgr);
+			return resp;
+		} catch(RuntimeException e) {
+			txCompleters.rollbackCloseSuppress(e, mgr, tx);
+			throw e; //rethrow
+		} finally {
+			Em.set(null);
+		}
+	}
 	
 	private void tryClose(EntityManager mgr, RuntimeException e) {
 		try {
@@ -46,6 +86,11 @@ public class TransactionHelper {
 
 	public <Resp> Resp runTransaction(Function<EntityManager, Resp> function) {
 		EntityManager mgr = factory.createEntityManager();
+		
+		if(mgr.getTransaction().isActive()) {
+			throw new IllegalStateException("You cannot call runTransaction in a transaction");
+		}
+		
 		EntityTransaction tx = mgr.getTransaction();
 		tx.begin();
 		
@@ -64,5 +109,27 @@ public class TransactionHelper {
 	}
 
 
-	
+	public <Resp> Resp runTransaction(Supplier<Resp> function) {
+		EntityManager mgr = factory.createEntityManager();
+		
+		if(mgr.getTransaction().isActive()) {
+			throw new IllegalStateException("You cannot call runTransaction in a transaction");
+		}
+		
+		EntityTransaction tx = mgr.getTransaction();
+		tx.begin();
+		
+		Em.set(mgr);
+		
+		try {
+			Resp resp = function.get();
+			txCompleters.commit(tx, mgr);
+			return resp;
+		} catch(RuntimeException e) {
+			txCompleters.rollbackCloseSuppress(e, mgr, tx);
+			throw e; //rethrow
+		} finally {
+			Em.set(null);
+		}
+	}
 }
