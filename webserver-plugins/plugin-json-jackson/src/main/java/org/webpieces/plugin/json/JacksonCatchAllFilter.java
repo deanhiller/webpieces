da1adb78f6bf157b85c57cc30bb474e6af94050e
Zachary Reed <zreed@live.com>
2021-03-25 17:34:27 -0400

    Use the right Math function.

--- webserver-plugins/plugin-json-jackson/src/main/java/org/webpieces/plugin/json/JacksonCatchAllFilter.java
+++ webserver-plugins/plugin-json-jackson/src/main/java/org/webpieces/plugin/json/JacksonCatchAllFilter.java
@@ -56,7 +56,7 @@ public class JacksonCatchAllFilter extends RouteFilter<JsonConfig> {
 			}
 			
 			byte[] obj = (byte[]) meta.getCtx().getRequest().requestState.get(JSON_REQUEST_KEY);
-			String json = new String(obj, 0, Math.max(100, obj.length));
+			String json = new String(obj, 0, Math.min(obj.length, 100));
 			log.error("Request failed for json="+json+"\nInternal Server Error method="+meta.getLoadedController().getControllerMethod(), t);
 			return translateError(t);
 		} else {
