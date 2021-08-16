56a3251b15b4b24d7d288782a8a0be011c718ec5
Dean Hiller <dean>
2021-08-15 21:44:11 -0600

    let's try two pull requests

--- victor/app/src/main/java/victor/App.java
+++ victor/app/src/main/java/victor/App.java
@@ -9,6 +9,13 @@ public class App {
     }
 
     public static void main(String[] args) {
+
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+
     }
 }
