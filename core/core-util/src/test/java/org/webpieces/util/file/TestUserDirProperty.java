9e07c61355d642a0af501d57e062b80561b9f372
deanhiller <dean.hiller@gmail.com>
2021-08-16 21:19:31 -0600

    add some code to test (#27)
    Co-authored-by: Dean Hiller <dean>

--- core/core-util/src/test/java/org/webpieces/util/file/TestUserDirProperty.java
+++ core/core-util/src/test/java/org/webpieces/util/file/TestUserDirProperty.java
@@ -14,6 +14,9 @@ public class TestUserDirProperty {
 	@Test
 	public void testUserDirProp() {
 
+		int deanVar = 0;
+		System.out.println("deanVar="+deanVar);
+
 		File f = new File("src/test/resources/logback-test.xml");
 
 		//assert absolute path is correct
@@ -31,6 +34,6 @@ public class TestUserDirProperty {
 		
 		//since absolute path was correct, it should exist 
 		Assert.assertFalse(f2.exists());
-		
+
 	}
 }
