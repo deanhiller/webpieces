6b0ffce62c7848ad81287b5d0d641347256016da
Alex Sweeney <ajs1998@gmail.com>
2021-09-22 13:56:56 -0500

    Add a comment

--- webserver-plugins/plugin-hibernate/src/main/java/org/webpieces/plugin/hibernate/TxHelperConfig.java
+++ webserver-plugins/plugin-hibernate/src/main/java/org/webpieces/plugin/hibernate/TxHelperConfig.java
@@ -1,5 +1,9 @@
 package org.webpieces.plugin.hibernate;
 
+/**
+ * If you want to aggregate your transactions by service name, @Provide this in a Guice module and supply your
+ * service name. TransactionHelper will @Inject it and use the service name as an extra tag on transaction time metrics
+ */
 public class TxHelperConfig {
 
     private String serviceName = "unknown"; // Default to "unknown"
