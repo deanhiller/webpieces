36a494babfc062e88caf23deca3d58d7658be272
Alex Sweeney <ajs1998@gmail.com>
2021-09-23 15:21:04 -0500

    Port changes from master from my PR (#64)
    * Port changes from master from https://github.com/deanhiller/webpieces/pull/63
    
    * Undo unnecessary changes and use Current instead of Context

---
+++ webserver-plugins/plugin-hibernate/src/main/java/org/webpieces/plugin/hibernate/metrics/HibernateEntityMeterTags.java
@@ -0,0 +1,8 @@
+package org.webpieces.plugin.hibernate.metrics;
+
+public class HibernateEntityMeterTags {
+
+    public static final String ENTITY_NAME = "entity";
+    public static final String REQUEST = "request";
+
+}
