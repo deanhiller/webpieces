7f44beb9c7a7cc29bbb5602d9b416377c9b40924
deanhiller <dean.hiller@gmail.com>
2021-11-05 10:45:19 -0600

    support json ajax redirect in filters (#93)
    Co-authored-by: Dean Hiller <dean>

--- webserver/http-router/src/main/java/org/webpieces/router/impl/routeinvoker/ResponseProcessorContent.java
+++ webserver/http-router/src/main/java/org/webpieces/router/impl/routeinvoker/ResponseProcessorContent.java
@@ -9,6 +9,7 @@ import org.webpieces.ctx.api.RequestContext;
 import org.webpieces.router.api.controller.actions.Action;
 import org.webpieces.router.api.controller.actions.RenderContent;
 import org.webpieces.router.api.routes.MethodMeta;
+import org.webpieces.router.impl.actions.AjaxRedirectImpl;
 import org.webpieces.router.impl.dto.RenderContentResponse;
 import org.webpieces.router.impl.proxyout.ProxyStreamHandle;
 
@@ -21,9 +22,12 @@ public class ResponseProcessorContent implements Processor {
 
 	@Override
 	public CompletableFuture<Void> continueProcessing(MethodMeta meta, Action controllerResponse, ProxyStreamHandle handle) {
-		if(!(controllerResponse instanceof RenderContent)) {
-			throw new UnsupportedOperationException("Bug, a webpieces developer must have missed writing a "
-					+ "precondition check on content routes to assert the correct return types");
+		if(controllerResponse instanceof AjaxRedirectImpl) {
+			AjaxRedirectImpl redirect = (AjaxRedirectImpl) controllerResponse;
+			return handle.sendAjaxRedirect(redirect.getId(), redirect.getArgs());
+		} else if(!(controllerResponse instanceof RenderContent)) {
+			throw new UnsupportedOperationException("One of the your RouteFilters in your App has a bug " +
+					"in that it calls Actions.redirect or Actions.render which is only for html pages");
 		}
 		
 		return createContentResponse(meta, (RenderContent)controllerResponse, handle);
