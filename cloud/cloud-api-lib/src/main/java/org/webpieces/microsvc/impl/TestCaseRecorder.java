52e45853d7c9346b2d92161f595ac3ec24855f84
deanorderly <60440987+deanorderly@users.noreply.github.com>
2021-11-13 08:19:25 -0700

    first phase of recording information complete.  next phase is to spit out the java test code (#101)
    Co-authored-by: Dean Hiller <dean>

---
+++ cloud/cloud-api-lib/src/main/java/org/webpieces/microsvc/impl/TestCaseRecorder.java
@@ -0,0 +1,9 @@
+package org.webpieces.microsvc.impl;
+
+public interface TestCaseRecorder {
+
+    public static final String RECORDER_KEY = "recorder";
+
+    public void addEndpointInfo(EndpointInfo info);
+
+}
