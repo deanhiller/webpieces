6b518f0cee6c46024ba8e23b89570f6b4607d36a
Dean <dean@Deans-MacBook-Pro.local>
2022-02-24 06:39:40 -0700

    allow version to be specified

--- runPublishSonatype.sh
+++ runPublishSonatype.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+ver=$@
+res="${ver//[^.]}"
+if [ "${#res}" -lt 2 ]; then
+  echo "##################################"
+  echo "      Version must be X.Y.Z       "
+  echo "##################################"
+  exit 1
+fi
 
 #if sonatype fails, we can republish without running whole build with this 
-./gradlew --stacktrace -PprojVersion="2.1.27" -Pparallel=1 publishMavenJavaPublicationToSonatypeRepository -x test
+./gradlew --stacktrace -PprojVersion="$@" -Pparallel=1 publishMavenJavaPublicationToSonatypeRepository -x test
