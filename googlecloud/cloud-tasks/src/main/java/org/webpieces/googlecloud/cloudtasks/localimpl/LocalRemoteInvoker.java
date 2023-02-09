e465495a7571513c0875abaf99dd35444df09158
Dean Hiller <dean.hiller@tray.com>
2023-02-09 20:57:37 +0100

    last fix for context stuff

--- googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/localimpl/LocalRemoteInvoker.java
+++ googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/localimpl/LocalRemoteInvoker.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.webpieces.ctx.api.HttpMethod;
 import org.webpieces.googlecloud.cloudtasks.api.RemoteInvoker;
 import org.webpieces.googlecloud.cloudtasks.api.ScheduleInfo;
+import org.webpieces.util.context.Context;
 import org.webpieces.util.futures.XFuture;
 
 import javax.inject.Inject;
 import java.net.InetSocketAddress;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -27,25 +29,28 @@ public class LocalRemoteInvoker implements RemoteInvoker {
 
     @Override
     public XFuture<Void> invoke(InetSocketAddress addr, String path, HttpMethod httpMethod, String bodyAsText, ScheduleInfo info) {
+        Map<String, Object> copy = Context.copyContext();
         if(info.isScheduledInFuture()) {
             executorService.schedule(
-                    () -> pretendToBeCallFromGCPCloudTasks(addr, path, httpMethod, bodyAsText),
+                    () -> pretendToBeCallFromGCPCloudTasks(copy, addr, path, httpMethod, bodyAsText),
                     info.getTime(), info.getTimeUnit());
         } else {
             executorService.execute(
-                    () -> pretendToBeCallFromGCPCloudTasks(addr, path, httpMethod, bodyAsText)
+                    () -> pretendToBeCallFromGCPCloudTasks(copy, addr, path, httpMethod, bodyAsText)
             );
         }
 
         return XFuture.completedFuture(null);
     }
 
-    private void pretendToBeCallFromGCPCloudTasks(InetSocketAddress addr, String path, HttpMethod httpMethod, String bodyAsText) {
+    private void pretendToBeCallFromGCPCloudTasks(Map<String, Object> copy, InetSocketAddress addr, String path, HttpMethod httpMethod, String bodyAsText) {
+        Context.restoreContext(copy);
         Endpoint endpoint = new Endpoint(addr, httpMethod.toString(), path);
         XFuture<String> stringXFuture = client.sendHttpRequest(bodyAsText, endpoint);
         stringXFuture.exceptionally( (e) -> {
             log.error("Exception queueing the request", e);
             return null;
         });
+        Context.clear();
     }
 }
