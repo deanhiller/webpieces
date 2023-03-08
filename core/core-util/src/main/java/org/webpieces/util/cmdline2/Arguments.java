708e4765cdd41ee335ba46dc6ee936702fe429f0
dmytro-tray <110851263+dmytro-tray@users.noreply.github.com>
2023-03-07 23:28:20 -0600

    Add required arg or env var (#148)
    Co-authored-by: Dmytro <troshchukdmitry@gmail.com>

--- core/core-util/src/main/java/org/webpieces/util/cmdline2/Arguments.java
+++ core/core-util/src/main/java/org/webpieces/util/cmdline2/Arguments.java
@@ -7,8 +7,8 @@ import java.util.function.Supplier;
 public interface Arguments {
 
 	//IF multiple things read the same key, we will print out multiple help arguments
-	
-	/** 
+
+	/**
 	 * @param <T> The type the argument will be converted to
 	 * @param argumentKey The cmdLine key as in db.port=0 (db.port is the key)
 	 * @param defaultValue Default value if argumentKey is not found on command line
@@ -18,7 +18,7 @@ public interface Arguments {
 	 */
 	<T> Supplier<T> createOptionalArg(String argumentKey, String defaultValue, String help, Function<String, T> converter);
 
-	/** 
+	/**
 	 * @param <T> The type the argument will be converted to
 	 * @param argumentKey The cmdLine key as in db.port=0 (db.port is the key)
 	 * @param help The help to show the user when missing arguments (or too many arguments)
@@ -28,22 +28,32 @@ public interface Arguments {
 	<T> Supplier<T> createRequiredArg(String argumentKey, String help, Function<String, T> converter);
 
 	/**
+	 * @param <T> The type the argument will be converted to
+	 * @param argumentKey The cmdLine key as in db.port=0 (db.port is the key)
+	 * @param envVarName The env var key as in DB_PASSWORD=PASSOWRD (DB_PASSWORD is the key)
+	 * @param help The help to show the user when missing arguments (or too many arguments)
+	 * @param converter Converts the cmd line String into the correct type or fails with the Help message to the user
+	 * @return a function that will be called later to get the cmd line argument
+	 */
+	<T> Supplier<T> createRequiredArgOrEnvVar(String argumentKey, String envVarName, String help, Function<String, T> converter);
+
+	/**
 	 * Special case convience method converting :{port} or {host}:{port} to InetSocketAddress
 	 */
 	Supplier<InetSocketAddress> createOptionalInetArg(String argumentKey, String defaultValue, String help);
-	
+
 	/**
 	 * See if an optional key exists or not.  
 	 */
 	Supplier<Boolean> createDoesExistArg(String key, String help);
-	
+
 	/**
 	 * After all the modules/routesFiles/plugins call the above methods, the main program then
 	 * can call this method to check that the command line had no errors and all arguments that
 	 * need to exist actually exist
 	 */
 	void checkConsumedCorrectly();
-	
+
 	/**
 	 * If you run just construction to call all the consumeXXX methods, then you can call this
 	 * to print commandline help
