f8c13601620ee1a9cbce9aa344a8ba57d20bef2c
Dean Hiller <dean.hiller@tray.com>
2023-03-09 16:02:11 +0100

    fix the lazy load

--- googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/impl/RemoteInvokerImpl.java
+++ googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/impl/RemoteInvokerImpl.java
@@ -3,26 +3,31 @@ package org.webpieces.googlecloud.cloudtasks.impl;
 import com.google.inject.Inject;
 import org.webpieces.ctx.api.HttpMethod;
 import org.webpieces.googlecloud.cloudtasks.api.*;
+import org.webpieces.util.SingletonSupplier;
 import org.webpieces.util.context.Context;
 import org.webpieces.util.futures.XFuture;
 
+import javax.inject.Provider;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 
 public class RemoteInvokerImpl implements RemoteInvoker {
 
-    @Inject
-    private GCPTaskClient gcpTasksClient;
+    private SingletonSupplier<GCPTaskClient> gcpTasksClient;
 
     @Inject
-    public RemoteInvokerImpl(GCPTaskClient gcpTaskClient) {
-        this.gcpTasksClient = gcpTaskClient;
+    public RemoteInvokerImpl(Provider<GCPTaskClient> gcpTaskClient)
+    {
+        //constructors should never connect to remove services such
+        // that TestBasicStart.java keeps working
+        //instead, always have a start/stop method for a lifecycle (instead of constructor and stop method)
+        gcpTaskClient = () -> gcpTasksClient.get();
     }
 
     @Override
     public XFuture<Void> invoke(Method method, InetSocketAddress addr, String path, HttpMethod httpMethod, String bodyAsText, ScheduleInfo info) {
 
-        JobReference jobReference = gcpTasksClient.createTask(method, addr, httpMethod, path, bodyAsText, info);
+        JobReference jobReference = gcpTasksClient.get().createTask(method, addr, httpMethod, path, bodyAsText, info);
         Context.put(Constants.WEBPIECES_SCHEDULE_RESPONSE,jobReference);
 
         return XFuture.completedFuture(null);
