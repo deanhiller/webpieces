cd7216856901995a44459ba9452d66f51967ee9c
Dean Hiller <dean.hiller@gmail.com>
2023-04-09 08:28:52 -0600

    fix templates (#170)
    Co-authored-by: Dean Hiller <dean.hiller@tray.com>

--- webserver-templates/webpiecesServerBuilder/TemplateJsonMicroSvc/production/src/main/java/webpiecesxxxxxpackage/base/HeadersCtx.java
+++ webserver-templates/webpiecesServerBuilder/TemplateJsonMicroSvc/production/src/main/java/webpiecesxxxxxpackage/base/HeadersCtx.java
@@ -1,20 +1,27 @@
 package webpiecesxxxxxpackage.base;
 
 import org.webpieces.ctx.api.ClientServiceConfig;
+import org.webpieces.microsvc.api.MicroSvcHeader;
 import org.webpieces.microsvc.server.api.HeaderCtxList;
 import org.webpieces.util.context.PlatformHeaders;
 import webpiecesxxxxxpackage.Server;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HeadersCtx implements HeaderCtxList {
-    public static ClientServiceConfig createConfig(String svcName) {
-        return new ClientServiceConfig(new HeadersCtx(), svcName);
+
+    public static ClientServiceConfig createConfig(String appName) {
+        return new ClientServiceConfig(new HeadersCtx(), appName);
     }
 
-    @Override
     public List<PlatformHeaders> listHeaderCtxPairs() {
-        return new ArrayList<>();
+        List<PlatformHeaders> list = new ArrayList<>();
+        CompanyHeaders[] values = CompanyHeaders.values();
+        MicroSvcHeader[] values1 = MicroSvcHeader.values();
+        list.addAll(Arrays.asList(values));
+        list.addAll(Arrays.asList(values1));
+        return list;
     }
 }
