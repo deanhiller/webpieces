49b04039972a6b3263d8925731b662de398b9fc4
Dean Hiller <dean.hiller@tray.com>
2023-05-10 23:11:28 +0200

    fix

--- core/core-future/src/main/java/org/webpieces/util/SneakyThrow.java
+++ core/core-future/src/main/java/org/webpieces/util/SneakyThrow.java
@@ -1,8 +1,26 @@
 package org.webpieces.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+
 public class SneakyThrow {
 
     public static <T extends Throwable> RuntimeException sneak(Throwable t) throws T {
+        if(t instanceof ExecutionException) {
+            //very important to keep this ones stack trace so rethrow
+            throw (T)t;
+        }
+        while((t instanceof CompletionException) || (t instanceof InvocationTargetException)) {
+
+            if (t.getCause() == null) {
+                break;
+            }
+
+            t = t.getCause();
+
+        }
+
         throw (T)t;
     }
 }
