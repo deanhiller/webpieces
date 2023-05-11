f70b4f1a7917bc427ddf6c094962b58c0b7839c2
Dean Hiller <dean.hiller@tray.com>
2023-05-11 11:32:13 +0200

    make simple as it is losing important info

--- core/core-future/src/main/java/org/webpieces/util/SneakyThrow.java
+++ core/core-future/src/main/java/org/webpieces/util/SneakyThrow.java
@@ -7,20 +7,6 @@ import java.util.concurrent.ExecutionException;
 public class SneakyThrow {
 
     public static <T extends Throwable> RuntimeException sneak(Throwable t) throws T {
-        if(t instanceof ExecutionException) {
-            //very important to keep this ones stack trace so rethrow
-            throw (T)t;
-        }
-        while((t instanceof CompletionException) || (t instanceof InvocationTargetException)) {
-
-            if (t.getCause() == null) {
-                break;
-            }
-
-            t = t.getCause();
-
-        }
-
         throw (T)t;
     }
 }
