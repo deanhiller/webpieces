8d342d4d9553925251959c3dc441a5e34198d5ab
Dean Hiller <dean.hiller@gmail.com>
2023-06-12 01:00:51 -0600

    an executor with metrics by type and returns future as well so you ca… (#211)
    * an executor with metrics by type and returns future as well so you can register code to run when complete on the same thread pool
    
    * add extra tags
    
    * fix up the executor
    
    ---------
    
    Co-authored-by: Dean Hiller <dean.hiller@tray.com>

---
+++ core/core-util/src/main/java/org/webpieces/util/threading/FutureExecutor.java
@@ -0,0 +1,28 @@
+package org.webpieces.util.threading;
+
+import org.webpieces.util.futures.XFuture;
+
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public interface FutureExecutor {
+
+
+    <RESP> XFuture<RESP> execute(Supplier<RESP> function, Map<String, String> extraTags);
+
+    <T> ScheduledFuture<?> scheduleAtFixedRate(Runnable command,
+                                               long initialDelay,
+                                               long period,
+                                               TimeUnit unit,
+                                               Map<String, String> extraMetricTags
+    );
+
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command,
+                                                     long initialDelay,
+                                                     long delay,
+                                                     TimeUnit unit,
+                                                     Map<String, String> extraMetricTags
+    );
+}
---
+++ core/core-util/src/main/java/org/webpieces/util/threading/FutureExecutorImpl.java
@@ -0,0 +1,66 @@
+package org.webpieces.util.threading;
+
+import org.webpieces.metrics.Monitoring;
+import org.webpieces.util.futures.XFuture;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
+public class FutureExecutorImpl implements FutureExecutor {
+    private Monitoring monitoring;
+    private ScheduledExecutorService svc;
+
+    public FutureExecutorImpl(Monitoring monitoring, ScheduledExecutorService svc, String name) {
+        this.monitoring = monitoring;
+        this.svc = svc;
+
+        if(svc instanceof ThreadPoolExecutor) {
+            ThreadPoolExecutor exec = (ThreadPoolExecutor) svc;
+            BlockingQueue<Runnable> queue = exec.getQueue();
+            Map<String, String> tag = Map.of("executorName", name);
+            //every minute, metrics will call queue.size ->
+            monitoring.gauge("event.queue.size", tag, queue, (q) -> queue.size());
+        }
+    }
+
+    @Override
+    public <RESP> XFuture<RESP> execute(Supplier<RESP> function, Map<String, String> extraTags) {
+        Map<String, String> tags = formTags(function.getClass(), extraTags);
+        XFuture<RESP> future = new XFuture<>();
+
+        MetricsSupplier runnable = new MetricsSupplier(monitoring, function, future, tags);
+        monitoring.incrementMetric("event.queued", tags);
+        svc.execute(runnable);
+        return future;
+    }
+
+    private <RESP> Map<String, String> formTags(Class<?> clazz, Map<String, String> extraTags) {
+        Map<String, String> tags = new HashMap<>();
+        tags.put("type", clazz.getSimpleName());
+        if(extraTags != null) {
+            tags.putAll(extraTags);
+        }
+        return tags;
+    }
+
+    @Override
+    public <T> ScheduledFuture<?> scheduleAtFixedRate(Runnable command,
+                                                      long initialDelay,
+                                                      long period,
+                                                      TimeUnit unit,
+                                                      Map<String, String> extraTags) {
+        Map<String, String> tags = formTags(command.getClass(), extraTags);
+        MetricsRunnable r = new MetricsRunnable<Void>(monitoring, command, tags);
+        return svc.scheduleAtFixedRate(r, initialDelay, period, unit);
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit, Map<String, String> extraMetricTags) {
+        Map<String, String> tags = formTags(command.getClass(), extraMetricTags);
+        MetricsRunnable r = new MetricsRunnable<Void>(monitoring, command, tags);
+        return svc.scheduleAtFixedRate(r, initialDelay, delay, unit);
+    }
+
+}
