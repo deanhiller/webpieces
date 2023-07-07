3ebe8361a2734a449e31e4abefef02f93c640446
Dean Hiller <dean.hiller@gmail.com>
2023-07-07 03:40:35 -0600

    txfr context to next thread (#217)
    Co-authored-by: Dean Hiller <dean.hiller@tray.com>

--- core/core-util/src/main/java/org/webpieces/util/threading/MetricsRunnable.java
+++ core/core-util/src/main/java/org/webpieces/util/threading/MetricsRunnable.java
@@ -2,7 +2,9 @@ package org.webpieces.util.threading;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.webpieces.metrics.Monitoring;
+import org.webpieces.util.context.Context;
 import org.webpieces.util.futures.XFuture;
 
 import java.util.Map;
@@ -14,17 +16,27 @@ public class MetricsRunnable<RESP> implements Runnable {
 
     private final Monitoring monitoring;
     private final Runnable function;
+    private final Map<String, Object> context;
+    private final Map<String, String> loggingMdcMap;
     private Map<String, String> tags;
 
     public MetricsRunnable(Monitoring monitoring, Runnable function, Map<String, String> tags) {
         this.monitoring = monitoring;
         this.function = function;
         this.tags = tags;
+        context = Context.copyContext();
+        //hack mdc until we fix
+        loggingMdcMap = MDC.getCopyOfContextMap();
     }
 
     @Override
     public void run() {
         long startTime = System.currentTimeMillis();
+        Map<String, Object> previously = Context.getContext();
+        Map<String, String> previousLogMap = MDC.getCopyOfContextMap();
+        Context.setContext(context);
+        MDC.setContextMap(loggingMdcMap);
+
         try {
             monitoring.incrementMetric("event.count", tags);
 
@@ -38,6 +50,8 @@ public class MetricsRunnable<RESP> implements Runnable {
             log.error("Exception", e);
         } finally {
             monitoring.endTimer("event.time", tags, startTime);
+            Context.setContext(previously);
+            MDC.setContextMap(loggingMdcMap);
         }
     }
 
