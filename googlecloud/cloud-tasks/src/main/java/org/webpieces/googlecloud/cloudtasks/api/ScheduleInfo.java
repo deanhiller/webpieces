8d6e705fe5df96a245356e6c87ac6c00d5fbd01b
Dean Hiller <dean.hiller@gmail.com>
2023-08-02 22:22:40 -0600

    expose building task (#227)
    Co-authored-by: Dean Hiller <dean.hiller@tray.com>

--- googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/api/ScheduleInfo.java
+++ googlecloud/cloud-tasks/src/main/java/org/webpieces/googlecloud/cloudtasks/api/ScheduleInfo.java
@@ -1,12 +1,17 @@
 package org.webpieces.googlecloud.cloudtasks.api;
 
+import com.google.cloud.tasks.v2.HttpRequest;
+import com.google.cloud.tasks.v2.Task;
+
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class ScheduleInfo {
 
     private Long taskTimeoutSeconds;
     private boolean scheduledInFuture = false;
     private long time;
+    private Function<HttpRequest, Task> taskCreator;
 
     public ScheduleInfo(long epochMsToRun) {
         this.scheduledInFuture = true;
@@ -30,5 +35,13 @@ public class ScheduleInfo {
 
     public void setTaskTimeoutSeconds(Long taskTimeoutSeconds) {
         this.taskTimeoutSeconds = taskTimeoutSeconds;
+    }
+
+    public Function<HttpRequest, Task> getTaskCreator() {
+        return taskCreator;
+    }
+
+    public void setTaskCreator(Function<HttpRequest, Task> taskCreator) {
+        this.taskCreator = taskCreator;
     }
 }
