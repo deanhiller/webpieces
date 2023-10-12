2272ff1c14660e77bc8a9208d6fd4dcb8b7ed86b
Kamlesh Sharma <kamlesh.sharma@tray.com>
2023-10-12 21:48:30 +0530

    removed entry point from FilterConfig

--- cloud/server-scaffolding/src/main/java/org/webpieces/microsvc/server/api/FilterConfig.java
+++ cloud/server-scaffolding/src/main/java/org/webpieces/microsvc/server/api/FilterConfig.java
@@ -1,7 +1,6 @@
 package org.webpieces.microsvc.server.api;
 
 public class FilterConfig {
-    private boolean entryPoint;
     private String packageRegEx = ".*";
     private String secureRegEx = "^(?!.*\\bexpose\\b).*$";
     private boolean enableHealthCheckEndpoint = true;
@@ -14,12 +13,6 @@ public class FilterConfig {
 
     public FilterConfig(String secureRegEx) {
         this.secureRegEx = secureRegEx;
-        this.entryPoint = true;
-    }
-
-    public FilterConfig(String packageRegEx, boolean entryPoint) {
-        this.packageRegEx = packageRegEx;
-        this.entryPoint = entryPoint;
     }
 
     public FilterConfig() {}
@@ -47,14 +40,6 @@ public class FilterConfig {
 
     public void setEnableHealthCheckEndpoint(boolean enableHealthCheckEndpoint) {
         this.enableHealthCheckEndpoint = enableHealthCheckEndpoint;
-    }
-
-    public boolean isEntryPoint() {
-        return entryPoint;
-    }
-
-    public void setEntryPoint(boolean entryPoint) {
-        this.entryPoint = entryPoint;
     }
 
     public boolean isRecordingEnabled() {
