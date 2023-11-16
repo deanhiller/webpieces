6f870fd18f7056559c8c54d1fa59b654c2ab335b
Dean Hiller <dean.hiller@tray.com>
2023-11-16 14:04:43 +0200

    only use the hostwithport now

--- cloud/generate-httpclient/src/main/java/org/webpieces/microsvc/client/api/RESTClientCreator.java
+++ cloud/generate-httpclient/src/main/java/org/webpieces/microsvc/client/api/RESTClientCreator.java
@@ -9,7 +9,9 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 public class RESTClientCreator {
 
@@ -18,41 +20,6 @@ public class RESTClientCreator {
     @Inject
     public RESTClientCreator(Provider<HttpsJsonClientInvokeHandler> wrapperProvider) {
         this.wrapperProvider = wrapperProvider;
-    }
-
-    /**
-     * @deprecated
-     */
-    @Deprecated
-    public <T> T createClient(Class<T> apiInterface, InetSocketAddress addr) {
-        return createClient(apiInterface, addr, false, false);
-    }
-
-    /**
-     * @deprecated
-     */
-    @Deprecated
-    public <T> T createClientPubsub(Class<T> apiInterface, InetSocketAddress addr) {
-        return createClient(apiInterface, addr, true, false);
-    }
-
-    /**
-     * @deprecated
-     */
-    @Deprecated
-    public <T> T createClientHttp(Class<T> apiInterface, InetSocketAddress addr) {
-        return createClient(apiInterface, addr, false, true);
-    }
-
-    /**
-     * @deprecated
-     */
-    @Deprecated
-    public <T> T createClient(Class<T> apiInterface, InetSocketAddress addr, boolean createForPubSub, boolean forHttp) {
-        //in testing getHostName returns host if they constructed with host and ip if they constructod with IP
-        //ONLY if the DNS is remote.  If localhost, it will return localhost which is ok here.
-        HostWithPort newAddr = new HostWithPort(addr.getAddress().getHostName(), addr.getPort());
-        return createClient(apiInterface, newAddr, createForPubSub, forHttp);
     }
 
     public <T> T createClient(Class<T> apiInterface, HostWithPort addr) {
@@ -68,6 +35,14 @@ public class RESTClientCreator {
     }
 
     public <T> T createClient(Class<T> apiInterface, HostWithPort addr, boolean createForPubSub, boolean forHttp) {
+
+        //quick DNS check or fail
+        try {
+            InetAddress.getByName(addr.getHostOrIpAddress());
+        } catch (UnknownHostException e) {
+            throw new IllegalArgumentException("Host="+addr.getHostOrIpAddress()+" could not be found");
+        }
+
         HttpsJsonClientInvokeHandler invokeHandler = wrapperProvider.get();
         boolean hasUrlParams = apiInterface.getAnnotation(NotEvolutionProof.class) != null;
         invokeHandler.initialize(addr, hasUrlParams, forHttp);
