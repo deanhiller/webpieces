bb008a8b2267c7ce402c35189190fbf5c20c8413
Dean Hiller <dean.hiller@tray.com>
2024-01-06 10:48:37 +0200

    fix the scopes

--- googlecloud/plugin-google-auth/src/main/java/org/webpieces/googleauth/api/SaveUser.java
+++ googlecloud/plugin-google-auth/src/main/java/org/webpieces/googleauth/api/SaveUser.java
@@ -1,9 +1,13 @@
 package org.webpieces.googleauth.api;
 
+import org.webpieces.googleauth.client.api.FetchTokenResponse;
 import org.webpieces.googleauth.client.api.ProfileAndTokens;
+import org.webpieces.router.api.controller.actions.Redirect;
 import org.webpieces.util.futures.XFuture;
 
 public interface SaveUser {
     XFuture<Void> saveUserIfNotExist(ProfileAndTokens profile);
+
+    Redirect returnRedirectIfScopesInvalid(FetchTokenResponse resp);
 
 }
