27271f6ff0ab187df80ae41846cb0a01682f512e
Dean Hiller <dean.hiller@tray.com>
2024-03-09 11:06:17 +0200

    fix mdc to simply be putMagic now

--- core/core-future/src/main/java/org/webpieces/util/context/PlatformHeaders.java
+++ core/core-future/src/main/java/org/webpieces/util/context/PlatformHeaders.java
@@ -2,10 +2,20 @@ package org.webpieces.util.context;
 
 public interface PlatformHeaders {
 
+    /**
+     * In some cases, header name is not transferred so isWantTransferred exists.  It is
+     * for cases where we may store objects like a RequestContext everyone can access on
+     * request path
+     */
     public String getHeaderName();
 
     public String getLoggerMDCKey();
 
+    /**
+     * loggerMDC is null if not want logged
+     * @deprecated
+     */
+    @Deprecated
     public boolean isWantLogged();
 
     public boolean isWantTransferred();
